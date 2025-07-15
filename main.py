701ccfddcf4ab5f3b9a495fadf75654027f8c4f8
Ariel Reches <arielreches@PC203266.localdomain>
2025-07-15 11:04:01 -0400

    new app.py

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+import uvicorn
+import os
+from prometheus_client import Counter
+
+# Define Prometheus metrics
+REQUESTS = Counter("http_requests_total", "Total HTTP requests", ["endpoint"])
 
 app = FastAPI()
 
+
 @app.get("/")
-def read_root():
-    return {"message": "Hello from App Runner!"}
+def hello(request: Request):
+    REQUESTS.labels(endpoint="/").inc()
+    return "Hello World!"
+
+
+if __name__ == "__main__":
+    print("Starting webserver...")
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+        port=int(os.getenv("PORT", 8080)),
+        log_level=os.getenv("LOG_LEVEL", "info"),
+        proxy_headers=True
+    )
